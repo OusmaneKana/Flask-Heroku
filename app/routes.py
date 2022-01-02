1199573c6d80198629166bd843b8c268e918d4c5
Serigne Ousmane Kana Ciss <kanaman6@gmail.com>
2022-01-01 21:41:51 -0600

    Push before Heroku

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "<h1> Samay Niggaz</h1>"
+    return render_template("index.html", title='Home')
+
+
+
+@app.route("/login", methods =['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"Login requested for user {form.username.data}, remember_me = {form.remember_me.data}")
+        return redirect('/index')
+    return render_template("login.html", form=form )
